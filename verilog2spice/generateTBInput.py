f59c3a48dd1f341953c0fba05f8242b66cd0cf16
njfredri <njfredri@uark.edu>
2025-03-10 17:55:40 -0500

    added scripts from other repos

---
+++ verilog2spice/generateTBInput.py
@@ -0,0 +1,174 @@
+import os
+import json
+
+#TODO: decide whether or not to include the signal if it is an input or output
+
+
+def extractSignalNames(line: str, delimiter:str = '_bus*num*_'):
+    columns = line.split('|')
+    signals = {}
+    for col in columns:
+        words = col.strip().split('=')
+        signalName = words[0]
+        value = words[1]
+        bitsize = len(value)
+        if bitsize <= 1:
+            signals[signalName] = [signalName]
+        else:
+            signals[signalName] = []
+            for i in range(bitsize):
+                delim = delimiter.replace('*num*', str(i))
+                signalbit = signalName+delim
+                signals[signalName].append(signalbit)
+    return signals
+
+def extractSignalValues(line: str, signalDef: dict, ignoreLength = ["time"]):
+    columns = line.split('|')
+    sigvals = {}
+    for col in columns:
+        words = col.strip().split('=')
+        signalName = words[0]
+        value = words[1]
+        # bitsizecheck = len(signalDef[signalName])
+        bitsize = len(value)
+
+        if bitsize <= 1 or signalName.lower() in ignoreLength:
+            sigvals[signalName] = value
+        else:
+            for i in range(bitsize):
+                subsigname = signalDef[signalName][i]
+                sigvals[subsigname] = value[i]
+    return sigvals
+
+def removeSpacesNearEquals(string: str):
+    while '= ' in string:
+        string = string.replace('= ', '=')
+    while ' =' in string:
+        string = string.replace(' =', '=')
+    return string
+
+def extractModelValues(sp_model):
+    info = {}
+    model = open(sp_model)
+    lines = model.readlines()
+    model.close()
+    #get the nominal vdd
+    for line in lines:
+        # print(line)
+        if 'nom' in line.lower():
+            if 'vdd' in line.lower():
+                info['vdd'] = line.split('=')[1].strip()
+    #not efficient, but idc
+    model = open(sp_model)
+    full = model.read()
+    models = full.split('.model')[1:]
+    info['models'] = []
+    for model in models:
+        info['models'].append(modelInfo(model=model))
+    return info
+
+def modelInfo(model:str):
+    info = {}
+    modelc = str(model)
+    modelc = removeSpacesNearEquals(modelc)
+    words = modelc.split()
+    
+    for word in words: #loop through to find the various threshold voltages
+        if '=' in word:
+            # print(word)
+            if 'vth' in word.lower():
+                sides = word.split('=')
+                info['threshold_voltage'] = float(sides[-1]) 
+
+    info['name'] = words[0]
+    return info
+
+def generateTBInput(infile, outfile, sp_model, timestep, timescaleunits='ns'):
+    temp = open(infile)
+    lines = temp.readlines()
+    temp.close()
+
+    #extract the signal names
+    signaldef = extractSignalNames(lines[0])
+    signalValues = {}
+    for line in lines:
+        linevals = extractSignalValues(line, signaldef)
+        for key in linevals.keys():
+            if key in signalValues.keys():
+                signalValues[key].append(linevals[key])
+            else:
+                signalValues[key] = [linevals[key]]
+    tbinfo = {}
+    tbinfo['input_signals'] = {}
+    tbinfo['output_signals'] = {}
+    timescale = 1000
+    timeskey = ''
+    for key in signalValues.keys():
+        if 'time' in key.lower():
+            timeskey = key
+    newtime = []
+    ndigits = 2
+
+    #loop through and adjust the time scale (assume ps to ns)
+    for time in signalValues[timeskey]:
+        scaledtimestr = str(round(float(time)/float(timescale), ndigits))
+        #go through the number string and remove any unneccessary 0's 
+        if '.' in scaledtimestr:
+            endindex = len(scaledtimestr)-1
+            while (scaledtimestr[endindex]=='0') and (endindex > 0):
+                endindex -= 1
+                if scaledtimestr[endindex]=='.': #only change if you find the decimal point
+                    scaledtimestr = scaledtimestr[0:endindex]
+                    break
+        newtime.append(scaledtimestr)
+    #TODO: make the input signals automatic when file format is changed
+    input_signals = ['clk', 'x', 'y', 'ce_alu']
+
+    #loop through the keys and add each signal to the appropriate dictionary
+    for key in signalValues.keys():
+        if 'time' in key.lower(): continue #skip time signal
+        elif key in input_signals:
+            if key not in tbinfo['input_signals'].keys():
+                tbinfo['input_signals'][key] = []
+            #loop through and add the list to the dictionary
+            for i in range(len(signalValues[key])):
+                tbinfo['input_signals'][key].append({'time': newtime[i], key: signalValues[key][i]})
+            sorted_signals = sorted(tbinfo['input_signals'][key], key=lambda x: float(x['time'])) #sort by time
+            tbinfo['input_signals'][key] = sorted_signals
+        else:
+            if key not in tbinfo['output_signals'].keys():
+                tbinfo['output_signals'][key] = []
+            #loop through and add the list to the dictionary
+            for i in range(len(signalValues[key])):
+                tbinfo['output_signals'][key].append({'time': newtime[i], key: signalValues[key][i]})
+
+    #loop through the times and determine how long to simulate
+    sim_length = 0    
+    for time in newtime:
+        t = float(time)
+        if(t > sim_length):
+            sim_length = t
+    sim_length += 5 #add in a constant to hold out any changes
+    tbinfo['timeend'] = str(sim_length)
+    tbinfo['timestep'] = str(timestep)
+    tbinfo['timescale'] = timescaleunits
+
+    #add voltage info
+    minf = extractModelValues(sp_model)
+    tbinfo['VDD'] = minf['vdd']
+    tbinfo['VSS'] = '0'
+
+    #TODO: determine 1's voltage using thesholds
+    tbinfo['1_voltage'] = tbinfo['VDD']
+    tbinfo['0_voltage'] = tbinfo['VSS']
+
+    #TODO: make transition times variable
+    tbinfo['transition_values'] = {}
+    tbinfo['transition_values']['setup_time'] = '0.1'
+
+    #write the tbinfo to json
+    with open(outfile, 'w+') as out:
+        json.dump(tbinfo, out, indent=4)
+
+
+generateTBInput("Alu_times.txt", "output.json", "ptm_22nm_bulk_hp.l", timestep=0.5)
---
+++ verilog2spice/generateTestbench.py
@@ -0,0 +1,145 @@
+import os
+import json
+
+#max length of time (in nano seconds)
+MAXTime = 100000
+
+
+def signalToString(signal: dict, prev_signal: dict, timescale:str, logical_high, setup_time) -> str:
+    voltage = '0'
+    if signal['value'] == '1':
+        voltage = logical_high
+    signalstr = str(signal['time']) + timescale + ' ' + str(voltage) + 'V '
+    #if there was a change in value, a transition must be made
+    if signal['value'] != prev_signal['value']:
+        voltage = '0'
+        if prev_signal['value'] == '1':
+            voltage = logical_high
+        setupStart = round((float(signal['time']) - float(setup_time)), 2)
+        setupstr = str(setupStart) + timescale + ' ' + str(voltage) + 'V '
+        signalstr = str(setupstr) + signalstr
+    return signalstr
+
+def generatePWL(signalValues:list, timescale:str, logical_high, setup_time) -> str:
+    pwl = ''
+    #assume signals are sorted for now.
+    newsig = []
+    voltage = '0'
+    if signalValues[0]['value'] == '1':
+        voltage = logical_high
+    initialsignalstr = str(signalValues[0]['time']) + timescale + ' ' + str(voltage) + 'V '
+    newsig.append(initialsignalstr) #add the first signal
+    for i in range(1,len(signalValues)):
+        curstr = signalToString(signalValues[i], signalValues[i-1], timescale, logical_high, setup_time)
+        newsig.append(curstr)
+    pwl = 'PWL('
+    pwl += ('\n+ ').join(newsig)
+    pwl += (')')
+    return pwl
+
+def generateDUTInstance(tbinfo:dict, numSubcircuitInstances) -> str:
+    spice = open(tbinfo['circuit_input'])
+    lines = spice.readlines()
+    subcircuit_inst = ''
+    for line in lines:
+        if '.subckt' in line.lower():
+            if tbinfo['dut_name'] in line:
+                ports = line.split()[2:]
+                name = 'X' + str(numSubcircuitInstances)
+                subcircuit_inst = name + ' ' + ' '.join(ports) + ' ' + tbinfo['dut_name']
+                break
+    return subcircuit_inst
+
+def generateDUTPortsList(tbinfo: dict) -> list:
+    spice = open(tbinfo['circuit_input'])
+    lines = spice.readlines()
+    subcircuit_inst = ''
+    for line in lines:
+        if '.subckt' in line.lower():
+            if tbinfo['dut_name'] in line:
+                ports = line.split()[2:]
+                finalports = []
+                for port in ports:
+                    if port.lower()=='vdd': continue
+                    elif port.lower()=='vss': continue
+                    else: finalports.append(port)
+                return finalports
+    return []
+
+def generateHspiceTestbench(input, tbout):
+    temp = open(input)
+    tbinfo = json.load(temp)
+    temp.close()
+
+    tbLines = []
+    numSubcircuitInstances=1
+
+    #include the circuit file
+    dutName = tbinfo['dut_name']
+    tbLines.append('* HSPICE Testbench for ' + str(dutName))
+    tbLines.append('.include "' + str(tbinfo['circuit_input']) + '"')
+    #specify the power supply
+    vdd = tbinfo['VDD']
+    vdds = str(vdd)
+    vss = tbinfo['VSS']
+    vsss = str(vss)
+    if 'V' not in vdds:
+        vdds = vdds + 'V'
+    if 'V' not in vsss:
+        vsss = vsss + 'V'
+
+    tbLines.append('\n\n* Power Supply ')
+    tbLines.append('VDD VDD 0 ' + vdds)
+    tbLines.append('VSS VSS 0 ' + vsss)
+
+    #create an instance of the DUT
+    tbLines.append('\n* Instantiate the DUT')
+    tbLines.append(generateDUTInstance(tbinfo=tbinfo, numSubcircuitInstances=numSubcircuitInstances))
+    numSubcircuitInstances+=1
+
+
+    timescale = str(tbinfo['timescale'])
+    setuptime = tbinfo['transition_values']['setup_time']
+    print(tbLines)
+
+    count = 1
+    for key in tbinfo['input_signals'].keys():
+        signals = tbinfo['input_signals'][key]
+        print(key, signals)
+        pwl = generatePWL(signals, timescale, vdd, setuptime)
+        print(pwl)
+        full_pwl = 'V' + str(count) + ' ' + str(key) + ' 0 '+ pwl
+        tbLines.append('* Input stimuli for ' + key + ' using Piecewise Linear Voltage (PWL)')
+        tbLines.append(full_pwl)
+        count += 1
+        
+
+    timestep = str(tbinfo['timestep']) 
+    timeend = str(tbinfo['timeend'])
+    if timescale not in timestep: timestep+=timescale
+    if timescale not in timeend: timeend+=timescale
+
+    trans = "\n.tran " + timestep + ' ' + timeend
+    tbLines.append(trans + ' 0 ' + timestep)
+    # tbLines.append(".probe V(*) I(*)")
+    
+    #add probes to all of the ports
+    portslist = generateDUTPortsList(tbinfo)
+    probe = '.probe '
+    prints= '.print TRAN '
+    
+    for port in portslist:
+        portstr = 'V(' + str(port) + ')'
+        probe += portstr + ' '
+        prints += portstr + ' '
+    tbLines.append(probe)
+    tbLines.append(prints)
+    # tbLines.append(".print tran V(A) V(B)")
+    tbLines.append(".option post=2 probe")
+    tbLines.append(".end")
+
+    tbout = open(tbout, 'w+')
+    tbout.write('\n'.join(tbLines))
+
+if __name__ == '__main__':
+    generateHspiceTestbench('example.json', 'tb.sp')
